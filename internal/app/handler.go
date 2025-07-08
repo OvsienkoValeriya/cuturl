ae3d3fc2ab258863798ebc8cf3488a181b3e110b
OvsienkoValeriya <44091861+ovsienkovaleriya@users.noreply.github.com>
2025-07-08 22:59:23 +0200

    fix test

--- internal/app/handler.go
+++ internal/app/handler.go
@@ -73,7 +73,8 @@ func (u *URLShortener) getOrCreateShortURL(originalURL string) (string, int, err
 
 	err := u.repo.Save(entry)
 	if err == nil {
-		return shortID, http.StatusCreated, nil
+		fullShortURL, _ := url.JoinPath(config.Get().BaseURL, shortID)
+		return fullShortURL, http.StatusCreated, nil
 	}
 
 	if errors.Is(err, store.ErrUniqueViolation) {
@@ -82,7 +83,8 @@ func (u *URLShortener) getOrCreateShortURL(originalURL string) (string, int, err
 			u.logger.Errorf("conflict on save but failed to find existing url: %v", findErr)
 			return "", http.StatusInternalServerError, findErr
 		}
-		return existing.ShortURL, http.StatusConflict, nil
+		fullShortURL, _ := url.JoinPath(config.Get().BaseURL, existing.ShortURL)
+		return fullShortURL, http.StatusConflict, nil
 	}
 
 	return "", http.StatusInternalServerError, err
