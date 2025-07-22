291c644dcc4178dab8b5ff07825cf1a5646b33b6
OvsienkoValeriya <44091861+ovsienkovaleriya@users.noreply.github.com>
2025-07-22 19:03:45 +0200

    test 14 authmiddleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -14,7 +14,8 @@ const UserIDKey CtxKey = "userID"
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		userID, err := auth.GetUserIDFromRequest(r)
-		if err != nil || userID == "" {
+
+		if err != nil {
 			userID, sig := auth.GenerateToken()
 			http.SetCookie(w, &http.Cookie{
 				Name:     "auth_token",
@@ -22,9 +23,11 @@ func AuthMiddleware(next http.Handler) http.Handler {
 				Path:     "/",
 				HttpOnly: true,
 			})
-		} else {
-			ctx := context.WithValue(r.Context(), UserIDKey, userID)
-			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+
+		if userID == "" {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
 			return
 		}
 
